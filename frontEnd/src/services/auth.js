72276f05e4ab7c2632409ce2a3f3bedfd2d93c6c
Lilyana Ivanova <lilyanaivanova2118@gmail.com>
2024-02-01 16:48:28 +0200

    making post request

--- frontEnd/src/services/auth.js
+++ frontEnd/src/services/auth.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 export const login = (username, password) => {
   console.log(" Log in:", username, password);
+  const tempResult = {
+    $id: "1",
+    flag: true,
+    message: "Account created",
+  };
   const configuration = {
     headers: {
       "Content-Type": "application/json",
@@ -12,31 +17,52 @@ export const login = (username, password) => {
       password,
     },
   };
-  return axios(configuration).then((result) => {
-    console.log(result);
-    return result.data;
-  });
+  return tempResult;
+  // return axios(configuration).then((result) => {
+  //   console.log(result);
+  //   return result.data;
+  // });
 };
 export const signup = (fullname, username, password) => {
   console.log("Sign up:", fullname, username, password);
-  const tempResult = {
-    $id: "1",
-    flag: true,
-    message: "Account created",
-  };
-  const configuration = {
+  const axios = require("axios");
+  let data = JSON.stringify({
+    email: "user@example.com",
+    password: "string",
+    fullName: "string",
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "https://localhost:7204/authentication/register",
     headers: {
       "Content-Type": "application/json",
     },
-    method: "post",
-    url: "http://localhost:7204/authentication/register",
-    data: {
-      fullname,
-      email: username,
-      password,
-    },
+    data: data,
   };
-  return tempResult;
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  // const configuration = {
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   method: "post",
+  //   url: "http://localhost:7204/authentication/register",
+  //   data: {
+  //     fullname,
+  //     email: username,
+  //     password,
+  //   },
+  // };
+
   // axios(configuration).then((result) => {
   //   console.log(result);
   //   return result.data;
